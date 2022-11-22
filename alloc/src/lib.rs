fbfb71b9c3b880bb4dced9efe6c226e6c955a7e9
Nick Santana <nick@mobilecoin.com>
2022-11-22 15:55:30 -0800

    Add macro to declare global allocator

--- alloc/src/lib.rs
+++ alloc/src/lib.rs
@@ -7,3 +7,24 @@
 extern crate alloc;
 
 mod allocator;
+
+pub use crate::allocator::Allocator;
+
+/// Defines a global allocator for use in an SGX enclave.
+///
+/// This should only be used in one place in the enclave binary.
+///
+/// The macro takes one argument, the name of the static allocator object.
+///
+/// # Example
+///
+/// ```
+/// mc_sgx_alloc::allocator!(ALLOCATOR_NAME);
+/// ```
+#[macro_export]
+macro_rules! allocator {
+    ($name:ident) => {
+        #[global_allocator]
+        static $name: $crate::Allocator = $crate::Allocator;
+    };
+}
