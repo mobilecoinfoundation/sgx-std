b3ecd3a63669c7ea0f851f88ccbe2f262bdaedf2
Nick Santana <nick@mobilecoin.com>
2023-01-10 13:11:19 -0800

    Add `panicking::panic_count` to `mc-sgx-panic`

--- panic/src/lib.rs
+++ panic/src/lib.rs
@@ -1,4 +1,7 @@
 // Copyright (c) 2023 The MobileCoin Foundation
+#![feature(thread_local)]
 #![doc = include_str!("../README.md")]
 #![deny(missing_docs, missing_debug_implementations, unsafe_code)]
 #![no_std]
+
+mod panicking;
